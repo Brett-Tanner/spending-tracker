a3fc055aa9dee74d3aeb3294332412f682c6d2ac
Brett Tanner <bretttanner171@gmail.com>
2025-10-13 15:04:32 +0900

    Add TransactionRow styling/formatting

--- src/components/TransactionList/TransactionRow/TransactionRow.tsx
+++ src/components/TransactionList/TransactionRow/TransactionRow.tsx
@@ -1,3 +1,5 @@
+import { shortDate } from "../../../lib/shortDate";
+import { toYen } from "../../../lib/toYen";
 import type { Transaction } from "../../../types/transaction";
 import { CrossIcon } from "../../shared/Icons/CrossIcon";
 import { EditIcon } from "../../shared/Icons/EditIcon";
@@ -12,10 +14,10 @@ function onEdit() { }
 export function TransactionRow({ transaction }: TransactionRowProps) {
 	return (
 		<div className="transaction-row">
-			<p>{transaction.user[0]}</p>
-			<p className="date">{transaction.date.toISOString()}</p>
+			<p className="user">{transaction.user[0]}</p>
+			<p className="date">{shortDate(transaction.date)}</p>
 			<p className="description">{transaction.description}</p>
-			<p className="amount">{transaction.amount}</p>
+			<p className="amount">{toYen(transaction.amount)}</p>
 			<button
 				aria-label="Edit"
 				disabled={transaction.status !== "completed"}
